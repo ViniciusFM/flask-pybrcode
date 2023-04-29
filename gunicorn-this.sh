533c5c3ac532f16a45339a217df4ebdfefdb808a
Vinícius F. M <viniciusmacielf@gmail.com>
2023-04-29 13:57:18 -0300

    add gunicorn

--- gunicorn-this.sh
+++ gunicorn-this.sh
@@ -2,5 +2,6 @@
 THISPATH=$(realpath $0)
 THISDIR=$(dirname $THISPATH)
 cd $THISDIR
+source ./.env/bin/activate
 gunicorn -w 4 -b viniciusmacielf.pythonanywhere.com:80 flask-pybrcode:app --daemon
 echo "server running"
